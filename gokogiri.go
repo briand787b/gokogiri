8c65a627455dbe3242bb4bcbee3eef90762dde9b
Brian D'Ostilio <briand787b@gmail.com>
2018-04-26 10:53:27 -0400

    Update gokogiri.go

--- gokogiri.go
+++ gokogiri.go
@@ -8,8 +8,8 @@ and a simple DOM-like inteface allows for building up documents from scratch.
 package gokogiri
 
 import (
-	"github.com/moovweb/gokogiri/html"
-	"github.com/moovweb/gokogiri/xml"
+	"github.com/briand787b/gokogiri/html"
+	"github.com/briand787b/gokogiri/xml"
 )
 
 /*
